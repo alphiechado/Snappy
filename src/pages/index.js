5a5767c24ae30e96cb85d65462e22090cb957e1b
Alfred Chado <alfredchado2010@gmail.com>
2023-07-10 01:12:20 +0100

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
           quick and hassle-free!
                 </p>
                 <div className='flex items-center self-start mt-2 lg:self-center'>
-                  <Link href="/dummy.pdf" target={"_blank"}
+                  <Link href="/" target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                   border-2 border-solid border-transparent hover:border-dark
